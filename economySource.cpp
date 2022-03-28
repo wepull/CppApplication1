0df885d15706472d6c127f8827b3c22574f2e022
Canaror33 <78983256+Canaror33@users.noreply.github.com>
2022-03-27 18:45:54 -0600

    Update economySource.cpp

--- economySource.cpp
+++ economySource.cpp
@@ -4,12 +4,109 @@
 #include <stdio.h>
 #include <string.h>
 
-int modifData(char mode,
-              int *money,
-              int *lvl,
-              int *exp,
-              int *inventory,
-              int inventory_S);
+void buy(int *money, int *inventory, const char *item,
+         int  price, int *amount,    int        *item_Id, int lvl)
+{
+    if ((*item_Id) > 1 || (*item_Id) < 4)
+    {
+        if (lvl < 2)
+        {
+            printf("Level 2+ requiered\n");
+            return;
+        }
+    }
+    if ((*money) < price)
+    {
+        printf("Not enough money\n");
+        return;
+    }
+                
+    if ((*money) < (price * (*amount)))
+    {
+        printf("Unable to buy colect $%d more to buy it x%d\n",
+               (price * (*amount)) - (*money), (*amount));
+        return;
+    }
+    
+    if (inventory[*item_Id] == -1) inventory[*item_Id] == 0;
+    inventory[*item_Id] += *amount;
+    *money        -= (*amount) * price;
+                
+    printf("Buied %d %s(s) -%d\n", *amount, item, price * (*amount));
+    return;
+}
+
+
+int modifData(char mode, int *money,     int *lvl,
+              int *exp,  int *inventory, int  inventory_S)
+{
+    FILE *dataFPtr;
+    if (mode == 's')
+    {
+        dataFPtr = fopen("player.esav", "wb");
+        
+        fwrite(money, sizeof(int), 1, dataFPtr);
+        fwrite(lvl,   sizeof(int), 1, dataFPtr);
+        fwrite(exp,   sizeof(int), 1, dataFPtr);
+        
+        fwrite(inventory, sizeof(int),inventory_S, dataFPtr);
+        printf("Saved data\n");
+    }
+    else if (mode == 'l')
+    {
+        dataFPtr = fopen("player.esav", "rb");
+        
+        fread(money, sizeof(int), 1, dataFPtr);
+        fread(lvl,   sizeof(int), 1, dataFPtr);
+        fread(exp,   sizeof(int), 1, dataFPtr);
+        
+        fread(inventory, sizeof(int), inventory_S, dataFPtr);
+        printf("Loaded data\n");
+    }
+    else
+    {
+        char *c_Reset = new char[4];
+        
+        char *c_Key   = new char[4];
+        printf("Do you want to reset you data\n"
+               "IT CAN BE UNDONE!\nyes|no: ");
+               
+        scanf("%s", c_Reset);
+        sscanf(c_Reset, "%s", c_Key);
+        
+        if (strcmp(c_Key, "yes") != 0)
+        {
+            printf("Cancelled\n");
+            fclose(dataFPtr);
+            delete[] c_Reset;
+            delete[] c_Key;
+        }
+        int d_Inventory[inventory_S];
+        d_Inventory[0] = 0;
+        
+        int *num = new int;
+        
+        for (int i = 1; i < inventory_S; i++) d_Inventory[i] = -1;
+        dataFPtr = fopen("player.esav", "wb");
+        *num = 10;
+        
+        fwrite(num, sizeof(int), 1, dataFPtr);
+        *num = 1;
+        
+        fwrite(num, sizeof(int), 1, dataFPtr);
+        *num = 0;
+        
+        fwrite(num, sizeof(int), 1, dataFPtr);
+        
+        fwrite(d_Inventory, sizeof(int), inventory_S, dataFPtr);
+        fclose(dataFPtr);
+        printf("Reseted now closing...\n");
+        return 1;
+    }
+    fclose(dataFPtr);
+    return 0;
+}
+
 
 int main()
 {
@@ -142,19 +239,19 @@ int main()
             int  *amount  = new int;
             
             scanf("%s", item);
-            if (strcmp(item, "stick") == 0) *item_Id = 0;
+                 if (strcmp(item, "stick" ) == 0) *item_Id = 0;
             
-            else if (strcmp(item, "rock") == 0) *item_Id = 1;
+            else if (strcmp(item, "rock"  ) == 0) *item_Id = 1;
             
-            else if (strcmp(item, "wood") == 0) *item_Id = 2;
+            else if (strcmp(item, "wood"  ) == 0) *item_Id = 2;
 
-            else if (strcmp(item, "stone") == 0) *item_Id = 3;
+            else if (strcmp(item, "stone" ) == 0) *item_Id = 3;
 
             else if (strcmp(item, "planks") == 0) *item_Id = 4;
 
-            else if (strcmp(item, "coal") == 0) *item_Id = 5;
+            else if (strcmp(item, "coal"  ) == 0) *item_Id = 5;
 
-            else if (strcmp(item, "iron") == 0) *item_Id = 6;
+            else if (strcmp(item, "iron"  ) == 0) *item_Id = 6;
 
             else if (strcmp(item, "copper") == 0) *item_Id = 7;
 
@@ -165,7 +262,7 @@ int main()
                 delete[] item;
                 delete   amount;
                 delete   item_Id;
-                printf("Error: Unkown item '%s'", item);
+                printf("Error: Unkown item '%s'\n", item);
                 continue;
             }
             
@@ -184,195 +281,37 @@ int main()
             switch(*item_Id)
             {
               case 0:
-                if (money < 4)
-                {
-                    printf("Not enough money\n");
-                    delete[] item;
-                    delete   amount;
-                    continue;
-                }
-                
-                if (money < (4 * (*amount)))
-                {
-                    printf("Unable to buy colect $%d more to buy it x%d\n", (4 * (*amount)) - money, (*amount));
-                    delete[] item;
-                    delete   amount;
-                    delete   item_Id;
-                    continue;
-                }
-                inventory[0] += *amount;
-                money        -= (*amount) * 4;
-                
-                printf("Buied %d stick(s) -%d\n", *amount, 4 * (*amount));
+                buy(&money, inventory, item, 4, amount, item_Id, lvl);
                 break;
                 
               case 1:
-                if (money < 6)
-                {
-                    printf("Not enough money\n");
-                    delete[] item;
-                    delete   amount;
-                    continue;
-                }
-                
-                if (money < (6 * (*amount)))
-                {
-                    printf("Unable to buy colect $%d more to buy it x%d\n", (6 * (*amount)) - money, (*amount));
-                    delete[] item;
-                    delete   amount;
-                    delete   item_Id;
-                    continue;
-                }
-                inventory[1] += *amount;
-                money        -= (*amount) * 6;
-                
-                printf("Buied %d rock(s) -%d\n", *amount, 6 * (*amount));
+                buy(&money, inventory, item, 6, amount, item_Id, lvl);
                 break;
               
               case 2:
-                if (money < 12)
-                {
-                    printf("Not enough money\n");
-                    delete[] item;
-                    delete   amount;
-                    continue;
-                }
-                
-                if (money < (12 * (*amount)))
-                {
-                    printf("Unable to buy colect $%d more to buy it x%d\n", (12 * (*amount)) - money, (*amount));
-                    delete[] item;
-                    delete   amount;
-                    delete   item_Id;
-                    continue;
-                }
-                inventory[2] += *amount;
-                money        -= (*amount) * 12;
-                
-                printf("Buied %d wood(s) -%d\n", *amount, 12 * (*amount));
+                buy(&money, inventory, item, 12, amount, item_Id, lvl);
                 break;
-              
+                
               case 3:
-                if (money < 18)
-                {
-                    printf("Not enough money\n");
-                    delete[] item;
-                    delete   amount;
-                    continue;
-                }
-                
-                if (money < (18 * (*amount)))
-                {
-                    printf("Unable to buy colect $%d more to buy it x%d\n", (18 * (*amount)) - money, (*amount));
-                    delete[] item;
-                    delete   amount;
-                    delete   item_Id;
-                    continue;
-                }
-                inventory[3] += *amount;
-                money        -= (*amount) * 18;
-                
-                printf("Buied %d Stone(s) -%d\n", *amount, 18 * (*amount));
+                buy(&money, inventory, item, 18, amount, item_Id, lvl);
                 break;
-                    /*
-                    ID |Name    |Price |ID |Name    |Price
-                    1  |Stick   |$4    |2  |Rock    |$6
-                    3  |Wood    |$12   |4  |Stone   |$18
-                    5  |Planks  |$26   |6  |Coal    |$35
-                    7  |Iron    |$56   |8  |Copper  |$78
-                   */
-              
+                
               case 4:
-                if (money < 26)
-                {
-                    printf("Not enough money\n");
-                    delete[] item;
-                    delete   amount;
-                    continue;
-                }
-                
-                if (money < (26 * (*amount)))
-                {
-                    printf("Unable to buy colect $%d more to buy it x%d\n", (26 * (*amount)) - money, (*amount));
-                    delete[] item;
-                    delete   amount;
-                    delete   item_Id;
-                    continue;
-                }
-                inventory[4] += *amount;
-                money        -= (*amount) * 26;
-                
-                printf("Buied %d Planks(s) -%d\n", *amount, 26 * (*amount));
+                buy(&money, inventory, item, 26, amount, item_Id, lvl);
                 break;
               
               case 5:
-                if (money < 35)
-                {
-                    printf("Not enough money\n");
-                    delete[] item;
-                    delete   amount;
-                    continue;
-                }
-                
-                if (money < (35 * (*amount)))
-                {
-                    printf("Unable to buy colect $%d more to buy it x%d\n", (35 * (*amount)) - money, (*amount));
-                    delete[] item;
-                    delete   amount;
-                    delete   item_Id;
-                    continue;
-                }
-                inventory[5] += *amount;
-                money        -= (*amount) * 35;
-                
-                printf("Buied %d Coal(s) -%d\n", *amount, 35 * (*amount));
+                buy(&money, inventory, item, 35, amount, item_Id, lvl);
                 break;
               
               case 6:
-                if (money < 28)
-                {
-                    printf("Not enough money\n");
-                    delete[] item;
-                    delete   amount;
-                    continue;
-                }
-                
-                if (money < (28 * (*amount)))
-                {
-                    printf("Unable to buy colect $%d more to buy it x%d\n", (28 * (*amount)) - money, (*amount));
-                    delete[] item;
-                    delete   amount;
-                    delete   item_Id;
-                    continue;
-                }
-                inventory[6] += *amount;
-                money        -= (*amount) * 28;
-                
-                printf("Buied %d Iron(s) -%d\n", *amount, 28 * (*amount));
+                buy(&money, inventory, item, 56, amount, item_Id, lvl);
                 break;
               
               case 7:
-                if ((*amount) < 1 || money < 78)
-                {
-                    printf("Empty money or/and amount requests\n");
-                    delete[] item;
-                    delete   amount;
-                    continue;
-                }
-                
-                if (money < (78 * (*amount)))
-                {
-                    printf("Unable to buy colect $%d more to buy it x%d\n", (78 * (*amount)) - money, (*amount));
-                    delete[] item;
-                    delete   amount;
-                    continue;
-                }
-                inventory[7] += *amount;
-                money        -= (*amount) * 78;
-                
-                printf("Buied %d Copper(s) -%d\n", *amount, 78 * (*amount));
+                buy(&money, inventory, item, 78, amount, item_Id, lvl);
+                break;
             }
-            
             delete[] item;
             delete   amount;
             delete   item_Id;
@@ -385,79 +324,5 @@ int main()
         else printf("Error unknown command\n");
     }
     printf("[Program finished]");
-    return 0;
-}
-
-int modifData(char mode,
-              int *money,
-              int *lvl,
-              int *exp,
-              int *inventory,
-              int inventory_S)
-{
-    FILE *dataFPtr;
-    if (mode == 's')
-    {
-        dataFPtr = fopen("player.esav", "wb");
-        
-        fwrite(money, sizeof(int), 1, dataFPtr);
-        fwrite(lvl,   sizeof(int), 1, dataFPtr);
-        fwrite(exp,   sizeof(int), 1, dataFPtr);
-        
-        fwrite(inventory, sizeof(int),inventory_S, dataFPtr);
-        printf("Saved data\n");
-    }
-    else if (mode == 'l')
-    {
-        dataFPtr = fopen("player.esav", "rb");
-        
-        fread(money, sizeof(int), 1, dataFPtr);
-        fread(lvl,   sizeof(int), 1, dataFPtr);
-        fread(exp,   sizeof(int), 1, dataFPtr);
-        
-        fread(inventory, sizeof(int), inventory_S, dataFPtr);
-        printf("Loaded data\n");
-    }
-    else
-    {
-        char *c_Reset = new char[4];
-        
-        char *c_Key   = new char[4];
-        printf("Do you want to reset you data\n"
-               "IT CAN BE UNDONE!\nyes|no: ");
-               
-        scanf("%s", c_Reset);
-        sscanf(c_Reset, "%s", c_Key);
-        
-        if (strcmp(c_Key, "yes") != 0)
-        {
-            printf("Cancelled\n");
-            fclose(dataFPtr);
-            delete[] c_Reset;
-            delete[] c_Key;
-        }
-        int d_Inventory[inventory_S];
-        d_Inventory[0] = 0;
-        
-        int *num = new int;
-        
-        for (int i = 1; i < inventory_S; i++) d_Inventory[i] = -1;
-        dataFPtr = fopen("player.esav", "wb");
-        *num = 10;
-        
-        fwrite(num, sizeof(int), 1, dataFPtr);
-        *num = 1;
-        
-        fwrite(num, sizeof(int), 1, dataFPtr);
-        *num = 0;
-        
-        fwrite(num, sizeof(int), 1, dataFPtr);
-        
-        fwrite(d_Inventory, sizeof(int), inventory_S, dataFPtr);
-        fclose(dataFPtr);
-        printf("Reseted now closing...\n");
-        return 1;
-    }
-    fclose(dataFPtr);
     return 0;
 }
